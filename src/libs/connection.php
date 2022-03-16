c75427c48bb07bc87c31822f06fbbc0c64f5b597
Pratik Bhetwal <pratik.bh7@gmail.com>
2022-03-16 09:29:33 +0545

    initial commit

---
+++ src/libs/connection.php
@@ -0,0 +1,16 @@
+<?php
+function db(): PDO
+{
+    static $pdo;
+
+    if (!$pdo) {
+        return new PDO(
+            sprintf("mysql:host=%s;dbname=%s;charset=UTF8", DB_HOST, DB_NAME),
+            DB_USER,
+            DB_PASSWORD,
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+    }
+    
+    return $pdo;
+}
